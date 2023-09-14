d0b31736e7fdd6aa2652e85b8380c95816e70945
marlon <marlon.oliveira34@hotmail.com>
2023-09-14 11:28:19 -0300

    quarto commit

--- Javinha/POO_heranca/TestaCliente.java
+++ Javinha/POO_heranca/TestaCliente.java
@@ -2,24 +2,51 @@ package Javinha.POO_heranca;
 
 import Javinha.POO.Cliente;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class TestaCliente {
     public static void main(String[] args) {
-        PessoaFisica c1 = new PessoaFisica("Marlon","SaoPaulo",23,80,1.85,"555555666");
-        PessoaFisica c2 = new PessoaFisica("Katarina","SaoPaulo",55,67,1.75,"444333777");
-
-        PessoaJuridica c3 = new PessoaJuridica("Ltda","SaoPaulo",55,67,1.75,"12345678912345");
-        PessoaJuridica c4 = new PessoaJuridica("Ltda","SaoPaulo",55,67,1.75,"12345678915050");
-
-        c1.visualizar();
-        c2.visualizar();
-        c3.visualizar();
-        c4.visualizar();
-
-        ClienteMaximo c5 = new ClienteMaximo("Katarina","SaoPaulo",55,67,1.75);
-
-        c5.resgatarBeneficios();
-        c5.comprar();
+        Scanner ler = new Scanner(System.in);
+        boolean loop = true;
+        int opcao = 0;
 
 
+        do {
+            System.out.println("Digite a opção 1 para continuar, caso não digite 2");
+            opcao = ler.nextInt();
+
+            if (opcao == 1) {
+                try {
+                    PessoaFisica c1 = new PessoaFisica("Marlon", "SaoPaulo", 23, 80, 1.85, "555555666");
+                    PessoaFisica c2 = new PessoaFisica("Katarina", "SaoPaulo", 55, 67, 1.75, "444333777");
+
+                    PessoaJuridica c3 = new PessoaJuridica("Ltda", "SaoPaulo", 55, 67, 1.75, "12345678912345");
+                    PessoaJuridica c4 = new PessoaJuridica("Ltda", "SaoPaulo", 55, 67, 1.75, "12345678915050");
+
+                    c1.visualizar();
+                    c2.visualizar();
+                    c3.visualizar();
+                    c4.visualizar();
+
+                    ClienteMaximo c5 = new ClienteMaximo("Katarina", "SaoPaulo", 55, 67, 1.75);
+
+                    c5.resgatarBeneficios();
+                    c5.comprar();
+
+                    loop = false;
+                } catch (InputMismatchException e) {
+                    System.err.println("\nExceção: " + e);
+                    ler.nextLine();
+                    System.out.println("\nDigite uma opção válida!");
+                } finally {
+                    System.out.println("\nSempre serei executada!\n");
+                }
+            } else if (opcao == 2) {
+                System.exit(0);
+            } else {
+                throw new RuntimeException("Digite uma opção válida!");
+            }
+        } while (loop);
     }
 }
--- Javinha/POO_heranca/TestaFuncionario.java
+++ Javinha/POO_heranca/TestaFuncionario.java
@@ -2,22 +2,52 @@ package Javinha.POO_heranca;
 
 import Javinha.POO.Funcionario;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class TestaFuncionario {
     public static void main(String[] args) {
-        Gerente d1 = new Gerente("Marlon","Gerente",23,80,1.500,"4");
-        Gerente d2 = new Gerente("Katarina","Gerente",55,67,3.000,"9");
+        Scanner ler = new Scanner(System.in);
+        boolean loop = true;
+        int escolha = 0;
 
-        Vendedor d3 = new Vendedor("Rosivaldo","Vendedor",55,67,3.000,"60");
-        Vendedor d4 = new Vendedor("Edmarildo","Vendedor",55,67,3.000,"34");
+        do {
+            System.out.println("Digite a opção 1 para continuar, caso não digite 2");
+            escolha = ler.nextInt();
 
-        d1.visualizar();
-        d2.visualizar();
-        d3.visualizar();
-        d4.visualizar();
+            if (escolha == 1) {
+                try {
+                    Gerente d1 = new Gerente("Marlon", "Gerente", 23, 80, 1.500, "4");
+                    Gerente d2 = new Gerente("Katarina", "Gerente", 55, 67, 3.000, "9");
 
-        FuncionarioGeral d5 = new FuncionarioGeral("Katarina","Gerente",55,67,3.000);
+                    Vendedor d3 = new Vendedor("Rosivaldo", "Vendedor", 55, 67, 3.000, "60");
+                    Vendedor d4 = new Vendedor("Edmarildo", "Vendedor", 55, 67, 3.000, "34");
 
-        d5.receberSalario();
-        d5.descontarFerias();
+                    d1.visualizar();
+                    d2.visualizar();
+                    d3.visualizar();
+                    d4.visualizar();
+
+
+                    FuncionarioGeral d5 = new FuncionarioGeral("Katarina", "Gerente", 55, 67, 3.000);
+
+                    d5.receberSalario();
+                    d5.descontarFerias();
+
+                    loop = false;
+                } catch (InputMismatchException e) {
+                    System.err.println("\nExceção: " + e);
+                    ler.nextLine();
+                    System.out.println("\nDigite uma opção válida!");
+                } finally {
+                    System.out.println("\nSempre serei executada!\n");
+                }
+
+                } else if (escolha == 2) {
+                    System.exit(0);
+                } else {
+                throw new RuntimeException("Digite uma opção válida!");
+            }
+        } while (loop);
     }
 }
